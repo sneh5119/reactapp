7e2784e827976f797d922a7738e94bf0c2b23d69
sneh5119 <63977692+sneh5119@users.noreply.github.com>
2024-04-18 00:36:45 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+<>
   <App />
   <SmeeIntegration />
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
